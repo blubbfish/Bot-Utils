444c9914c4241d1650e56ab2edaa060177a35685
BlubbFish <git@blubbfish.net>
2018-06-09 21:27:15 +0000

    [NF] Moved CronJob, Overtaker, Statuspolling to Abstract Bot-Utils [NF] Create NSI Script to produce exe [NF] Add License.txt

---
+++ Moduls/Overtaker.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlubbFish.Utils.IoT.Bots.Moduls {
+  public abstract class Overtaker<T> : AModul<T>, IDisposable {
+    protected readonly Dictionary<String, Dictionary<String, String>> events = new Dictionary<String, Dictionary<String, String>>();
+
+    #region Constructor
+    public Overtaker(T lib, InIReader settings) : base(lib, settings) {
+      this.ParseIni();
+    }
+    #endregion
+
+    #region Overtakerfunctions
+    protected void ParseIni() {
+      foreach (KeyValuePair<String, Dictionary<String, String>> item in this.config) {
+        if (item.Value.ContainsKey("from")) {
+          String from = item.Value["from"];
+          String[] source = from.Split(':');
+          this.events.Add(source[0], item.Value);
+          this.LibraryUpdateHook(source[0]);
+        }
+      }
+    }
+
+    protected void SetValues(Object sender, String name, Dictionary<String, String> dictionary) {
+      String from = dictionary["from"];
+      String[] source = from.Split(':');
+      if (source.Length != 2) {
+        return;
+      }
+      String source_value;
+      if (sender.HasProperty(source[1])) {
+        source_value = sender.GetProperty(source[1]).ToString();
+      } else {
+        return;
+      }
+      if (dictionary.ContainsKey("convert")) {
+        foreach (String tuple in dictionary["convert"].Split(';')) {
+          String[] item = tuple.Split('-');
+          if (source_value == item[0]) {
+            source_value = item[1];
+          }
+        }
+      }
+      if (dictionary.ContainsKey("to")) {
+        foreach (String to in dictionary["to"].Split(';')) {
+          String[] target = to.Split(':');
+          if (target.Length == 2) {
+            this.SetValueHook(target[0], target[1], source_value);
+          }
+        }
+      }
+    }
+
+    protected abstract void LibraryUpdateHook(String id);
+
+    protected abstract void SetValueHook(String id, String prop, String value);
+    #endregion
+
+    #region AModul
+    public override void Interconnect(Dictionary<String, Object> moduls) { }
+    protected override void UpdateConfig() {
+      this.ParseIni();
+    }
+    #endregion
+
+    #region IDisposable Support
+    private Boolean disposedValue = false;
+
+    protected virtual void Dispose(Boolean disposing) {
+      if (!this.disposedValue) {
+        if (disposing) {
+        }
+        this.disposedValue = true;
+      }
+    }
+
+    public override void Dispose() {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+    #endregion   
+  }
+}
---
+++ Moduls/Statuspolling.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using BlubbFish.Utils.IoT.Bots.Interfaces;
+
+namespace BlubbFish.Utils.IoT.Bots.Moduls {
+  public abstract class Statuspolling<T> : AModul<T>, IDisposable, IForceLoad {
+
+    #region Constructor
+    public Statuspolling(T lib, InIReader settings) : base(lib, settings) { }
+    #endregion
+
+    #region Statuspollingfunctions
+    protected abstract void PollSpecific(Object obj);
+
+    protected abstract void PollAll(Object obj);
+    #endregion
+
+    #region AModul
+    public override void Interconnect(Dictionary<String, Object> moduls) {
+      foreach (KeyValuePair<String, Object> item in moduls) {
+        if (item.Value is CronJob<T>) {
+          ((AModul<T>)item.Value).SetInterconnection("0 0 * * *", new Action<Object>(this.PollAll), null);
+          if (this.config.Count != 0) {
+            foreach (KeyValuePair<String, Dictionary<String, String>> section in this.config) {
+              if (section.Value.ContainsKey("cron") && section.Value.ContainsKey("devices")) {
+                ((AModul<T>)item.Value).SetInterconnection(section.Value["cron"], new Action<Object>(this.PollSpecific), section.Value["devices"]);
+              }
+            }
+          }
+        }
+      }
+    }
+    protected override void UpdateConfig() { }
+    #endregion
+
+    #region IDisposable Support
+    private Boolean disposedValue = false;
+
+    protected virtual void Dispose(Boolean disposing) {
+      if (!this.disposedValue) {
+        if (disposing) {
+        }
+        this.disposedValue = true;
+      }
+    }
+
+    public override void Dispose() {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+    #endregion
+  }
+}
